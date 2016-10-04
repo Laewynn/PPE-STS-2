708edc48c1e960436c5fc3014e82b263a4dfc72d
Laëtitia Marchand <laetitiamarchand35@gmail.com>
2016-10-04 11:11:28 +0200

    Update CLIENT.php

--- CLIENT.php
+++ CLIENT.php
@@ -35,8 +35,10 @@ class Client {
 	* @param $villeclient = la ville du client
 	* @param $telephoneclient = le téléphone du client
 	* @param $mailcient = l'email du client
+	*
+	*@author ¨MARCHAND Laëtitia
+	*@date 04/10/16
 	*/
-	// Constructeur de la classe Client
 	function __construct($idclient, $nomclient, $prenomclient, $adresseclient, $cpclient, $villeclient, $telephoneclient, $mailclient) {
 		$this->idclient=$idclient;
 		$this->nomclient=$nomclient;
@@ -47,77 +49,196 @@ class Client {
 		$this->telephoneclient=$telephoneclient;
 		$this->MailClient=$mailclient;
 	}
-	// Accesseurs et Mutateurs de ID Client
+	
+	
+	/*
+	* Fonction qui sert à donner la valeur de l'id du client
+	* @return $idclient= l'identifiant du client
+	*
+	*@author ¨MARCHAND Laëtitia
+	*@date 04/10/16
+	*/
   	function getIDClient(){
   		return $this->$idclient;
   	}
 	
+	
+	/*
+	* Fonction qui sert à modifier la valeur de l'id du client
+	* @param $var = le nouvel identifiant du client
+	*
+	*@author ¨MARCHAND Laëtitia
+	*@date 04/10/16
+	*/
 	function setIDClient($var){
-  		return $this->idclient=$var;
+  		$this->idclient=$var;
   	}
 	
-	// Accesseurs et Mutateurs du Nom Client
+	
+	/*
+	* Fonction qui sert à donner la valeur de le nom du client
+	* @return $nomclient= le nom du client
+	*
+	*@author ¨MARCHAND Laëtitia
+	*@date 04/10/16
+	*/
   	function getNomClient(){
   		return $this->$nomclient;
   	}
 	
+	
+	/*
+	* Fonction qui sert à modifier la valeur du nom du client
+	* @param $var = le nouveau nom du client
+	*
+	*@author ¨MARCHAND Laëtitia
+	*@date 04/10/16
+	*/
 	function setNomClient($var){
-  		return $this->nomclient=$var;
+  		$this->nomclient=$var;
   	}
 	
-	// Accesseurs et Mutateurs du Prenom Client
+	
+	/*
+	* Fonction qui sert à donner la valeur de le prénom du client
+	* @return $prenomclient= le prénom du client
+	*
+	*@author ¨MARCHAND Laëtitia
+	*@date 04/10/16
+	*/
   	function getPrenomClient(){
   		return $this->$prenomclient;
   	}
 	
+	
+	/*
+	* Fonction qui sert à modifier la valeur du nom du client
+	* @param $var = le nouveau prénom client
+	*
+	*@author ¨MARCHAND Laëtitia
+	*@date 04/10/16
+	*/
 	function setPrenomClient($var){
-  		return $this->prenomclient=$var;
+  		$this->prenomclient=$var;
   	}
 	
-	// Accesseurs et Mutateurs de l'Adresse du Client
+	
+	/*
+	* Fonction qui sert à donner la valeur de l'adresse du client
+	* @return $adresseclient= l'adresse du client
+	*
+	*@author ¨MARCHAND Laëtitia
+	*@date 04/10/16
+	*/
   	function getAdresseClient(){
   		return $this->$adresseclient;
   	}
 	
+	
+	/*
+	* Fonction qui sert à modifier la valeur de l'adresse du client
+	* @param $var = la nouvelle adresse du client
+	*
+	*@author ¨MARCHAND Laëtitia 
+	*@date 04/10/16
+	*/
 	function setAdresseClient($var){
-  		return $this->adresseclient=$var;
+  		$this->adresseclient=$var;
   	}
 	
-	// Accesseurs et Mutateurs du Code Postal du Client
+	
+	/*
+	* Fonction qui sert à donner la valeur de le code postal du client
+	* @return $cpclient= le code postal du client
+	*
+	*@author ¨MARCHAND Laëtitia
+	*@date 04/10/16
+	*/
   	function getCPClient(){
   		return $this->$cpclient;
   	}
 	
+	
+	/*
+	* Fonction qui sert à modifier la valeur du code postal du client
+	* @param $var = le nouveau code postal du client
+	*
+	*@author ¨MARCHAND Laëtitia 
+	*@date 04/10/16
+	*/
 	function setCPClient($var) {
-  		return $this->$cpclient=$var;
+  		$this->$cpclient=$var;
   	}
 	
-	// Accesseurs et Mutateurs de la Ville du Client
+	/*
+	* Fonction qui sert à donner la valeur de la ville du client
+	* @return $villeclient= la ville du client
+	*
+	*@author ¨MARCHAND Laëtitia
+	*@date 04/10/16
+	*/
   	function getVilleClient(){
   		return $this->$villeclient;
   	}
 	
+	
+	/*
+	* Fonction qui sert à modifier la valeur la ville du client
+	* @param $var = le nouveau code postal du client
+	*
+	*@author ¨MARCHAND Laëtitia 
+	*@date 04/10/16
+	*/
 	function setVilleClient($var){
-  		return $this->$villeclient=$var;
+  		$this->$villeclient=$var;
   	}
 	
 	
-	// Accesseurs et Mutateurs du Telephone du Client
+	/*
+	* Fonction qui sert à donner la valeur du numéro de téléphone du client
+	* @return $telephoneclient= le numéro de téléphone du client
+	*
+	*@author ¨MARCHAND Laëtitia
+	*@date 04/10/16
+	*/
   	function getTelephoneClient(){
   		return $this->$telephoneclient;
   	}
 	
+	
+	/*
+	* Fonction qui sert à modifier la valeur du numéro de téléphone du client
+	* @param $var = le nouveau numéro de téléphone du client
+	*
+	*@author ¨MARCHAND Laëtitia 
+	*@date 04/10/16
+	*/
 	function setTelephoneClient($var){
-  		return $this->$telephoneclient=$var;
+  		$this->$telephoneclient=$var;
   	}
 	
-	// Accesseurs et Mutateurs du mail du Client
+	
+	/*
+	* Fonction qui sert à donner la valeur de l'email du client
+	* @return $telephoneclient= l'email du client
+	*
+	*@author ¨MARCHAND Laëtitia
+	*@date 04/10/16
+	*/
   	function getMailClient(){
   		return $this->$mailclient;
   	}
 	
+	
+	/*
+	* Fonction qui sert à modifier la valeur de l'email du client
+	* @param $var = le nouvel email du client
+	*
+	*@author ¨MARCHAND Laëtitia 
+	*@date 04/10/16
+	*/
 	function setMailClient($var){
-  		return $this->$mailclient=$var;
+  		 $this->$mailclient=$var;
   	}	
   	
 }
